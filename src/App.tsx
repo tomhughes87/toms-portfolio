ff636272735d162d066160915bb66be2e7e06546
Tom Hughes <tomhughes1987@gmail.com>
2022-08-23 13:11:39 +0100

    set up react router and add place holder pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Aboutme from './pages/aboutme/Aboutme';
+import Contact from './pages/contact/Contact';
+import Projects from './pages/projects/Projects';
+import Skills from './pages/skills/Skills';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={ />}> */}
+            <Route index element={<Contact />} />
+            
+            <Route path="Projects" element={<Projects />} />
+            <Route path="Project" element={<Projects />} />
+            <Route path="myProjects" element={<Projects />} />
+            <Route path="myProject" element={<Projects />} />
+
+            <Route path="about" element={<Aboutme />} />
+            <Route path="aboutme" element={<Aboutme />} />
+            
+            <Route path="contact" element={<Contact />} />
+            <Route path="contacts" element={<Contact />} />
+            <Route path="contactme" element={<Contact />} />
+
+            <Route path="skills" element={<Skills />} />
+            <Route path="skill" element={<Skills />} />
+            <Route path="myskills" element={<Skills />} />
+            <Route path="myskill" element={<Skills />} />
+
+
+
+            {/* <Route path="*" element={<NoPage />} /> */}
+
+          {/* </Route> */}
+        </Routes>
+      </BrowserRouter>
+    // );
+  // }
   );
 }
+
 
 export default App;
