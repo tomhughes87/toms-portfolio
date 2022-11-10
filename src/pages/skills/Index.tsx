8d996717059933e860794c9493942683776745cf
Tom Hughes <tomhughes1987@gmail.com>
2022-11-11 00:04:44 +0100

    add skills page with skill compo + logos

---
+++ src/pages/skills/Index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Skills from '../../components/Skills'
+import './index.css'
+
+export default function SkillsPage() {
+  return (
+    // <div>Skills</div>
+
+    <Skills/>
+  )
+}
