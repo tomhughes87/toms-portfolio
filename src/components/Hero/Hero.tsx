e974c5940b0777c6c8aa60ccbac6f65756fc44c3
Tom Hughes <tomhughes1987@gmail.com>
2022-11-11 14:54:48 +0100

    add a hero image compo and add it to contact page

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Hero.css";
+
+export default function Hero() {
+  return (
+    <div id="container-hero">
+      <h1 id="text-hero">Need to contact me?</h1>
+    </div>
+  );
+}
--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
+import Hero from "../../components/Hero/Hero";
 
 export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <Hero />
+    </div>
+  );
 }
