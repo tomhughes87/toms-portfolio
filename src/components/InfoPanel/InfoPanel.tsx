e572c40b49b1b3841dcd6c8ce4fe6a15e0ebf850
Tom Hughes <tomhughes1987@gmail.com>
2022-11-12 18:31:11 +0100

    refac reveal scroll, start making responsive (wip)

--- src/components/InfoPanel/InfoPanel.tsx
+++ src/components/InfoPanel/InfoPanel.tsx
@@ -3,23 +3,7 @@ import "./style.css";
 import pic from "./patrick-tomasso-QMDap1TAu0g-unsplash.jpg";
 
 export default function InfoPanel() {
-  // function handleClick(e: any) {
-  //   const rightPanel = document.getElementById("rightpanel");
-  //   const leftPanel = document.getElementById("img-scroll-reveal");
-
-  //   if (rightPanel?.classList.contains("show-right")) {
-  //     rightPanel?.classList.remove("show-right");
-  //     leftPanel?.classList.remove("show-left");
-  //   } else {
-  //     rightPanel?.classList.add("show-right");
-  //     leftPanel?.classList.add("show-left");
-  //   }
-  // }
-
   useEffect(() => {
-    // let li: NodeListOf<HTMLElement> = document.querySelectorAll(
-    //   "#container-scroll-reveal"
-    // );
     let li: NodeListOf<HTMLElement> =
       document.querySelectorAll(".observe-this");
     console.log(li);
@@ -27,11 +11,7 @@ export default function InfoPanel() {
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
-          console.log(entry.target);
-          console.log("THIS THING:", entry.target);
-
-          entry.target.classList.toggle("show-right", entry.isIntersecting);
-          // entry.target.classList.toggle("show-left", entry.isIntersecting);
+          entry.target.classList.toggle("reveal", entry.isIntersecting);
         });
       },
       {
@@ -47,172 +27,56 @@ export default function InfoPanel() {
 
   return (
     <>
-      <div id="container-scroll-reveal">
-        <div>
-          <img
-            id="img-scroll-reveal"
-            className="scroll-reveal-element left-scroll-reveal-start observe-this"
-            src={pic}
-            alt=""
-          />
-        </div>
+      <div id="testdiv-length">
+        <div id="container-scroll-reveal">
+          <div id="sub-container-scroll-reveal">
+            <img
+              // id="img-scroll-reveal"
+              className="scroll-reveal-element left-scroll-reveal-start observe-this"
+              src={pic}
+              alt=""
+            />
+          </div>
 
-        <div>
-          <h1
-            id="testing-text"
-            className="scroll-reveal-element right-scroll-reveal-start observe-this "
-          >
-            Title
-          </h1>
-          <p
-            id="testing-text"
-            className="scroll-reveal-element right-scroll-reveal-start observe-this "
-          >
-            I am playing with this InfoPanel
-          </p>
-          <p
-            id="testing-text"
-            className="scroll-reveal-element right-scroll-reveal-start observe-this "
-          >
-            I am playing with this InfoPanel
-          </p>
-          <p
-            id="testing-text"
-            className="scroll-reveal-element right-scroll-reveal-start observe-this "
-          >
-            I am playing with this InfoPanel
-          </p>
-          <p
-            id="testing-text"
-            className="scroll-reveal-element right-scroll-reveal-start observe-this "
-          >
-            I am playing with this InfoPanel
-          </p>
+          <div id="sub-container-scroll-reveal">
+            <h1
+              id="testing-text"
+              className="scroll-reveal-element right-scroll-reveal-start observe-this "
+            >
+              Title
+            </h1>
+
+            <p className="scroll-reveal-element right-scroll-reveal-start observe-this ">
+              Ukrainian soldiers have been welcomed into Kherson by jubilant{" "}
+            </p>
+            <p className="scroll-reveal-element right-scroll-reveal-start observe-this ">
+              residents, after Russia said it had fully withdrawn from the key
+            </p>
+            <p className="scroll-reveal-element right-scroll-reveal-start observe-this ">
+              southern city. Video showed locals on the streets, flying
+              Ukraine's
+            </p>
+            <p className="scroll-reveal-element right-scroll-reveal-start observe-this ">
+              national flag and chanting as Kyiv's troops arrived. Ukrainian
+              President
+            </p>
+            <p className="scroll-reveal-element right-scroll-reveal-start observe-this ">
+              Volodymyr Zelensky said Kherson was "our city". Kherson was the
+              only
+            </p>
+            <p className="scroll-reveal-element right-scroll-reveal-start observe-this ">
+              regional capital taken by Russia after February's invasion. The
+              retreat
+            </p>
+            <p className="scroll-reveal-element right-scroll-reveal-start observe-this ">
+              has been seen as one of its biggest setbacks of the war.
+            </p>
+            <button className="scroll-reveal-element btn-reveal-start observe-this ">
+              Portfolio
+            </button>
+          </div>
         </div>
       </div>
-      {/* 
-      
-
-
-
-
-      */}
-      <div id="container-scroll-reveal" className="2">
-        <div
-          id="leftpanel"
-          className="scroll-reveal-element left-scroll-reveal-start"
-        >
-          <img id="img-scroll-reveal" src={pic} alt="" />
-        </div>
-
-        <div
-          id="rightpanel"
-          className="scroll-reveal-element right-scroll-reveal-start "
-        >
-          <p>right InfoPanel</p>
-        </div>
-      </div>
-      <div id="container-scroll-reveal" className="3">
-        <div
-          id="leftpanel"
-          className="scroll-reveal-element left-scroll-reveal-start"
-        >
-          <img id="img-scroll-reveal" src={pic} alt="" />
-        </div>
-
-        <div
-          id="rightpanel"
-          className="scroll-reveal-element right-scroll-reveal-start"
-        >
-          <p>right InfoPanel</p>
-        </div>
-      </div>
-      <div id="container-scroll-reveal" className="4">
-        <div
-          id="leftpanel"
-          className="scroll-reveal-element left-scroll-reveal-start"
-        >
-          <img id="img-scroll-reveal" src={pic} alt="" />
-        </div>
-
-        <div
-          id="rightpanel"
-          className="scroll-reveal-element right-scroll-reveal-start"
-        >
-          <p>
-            Ukrainian soldiers have been welcomed into Kherson by jubilant
-            residents, after Russia said it had fully withdrawn from the key
-            southern city. Video showed locals on the streets, flying Ukraine's
-            national flag and chanting as Kyiv's troops arrived. Ukrainian
-            President Volodymyr Zelensky said Kherson was "our city". Kherson
-            was the only regional capital taken by Russia after February's
-            invasion. The retreat has been seen as one of its biggest setbacks
-            of the war.
-          </p>
-        </div>
-      </div>
-      <div id="container-scroll-reveal">
-        <div
-          id="leftpanel"
-          className="scroll-reveal-element left-scroll-reveal-start "
-        >
-          <img id="img-scroll-reveal" src={pic} alt="" />
-        </div>
-
-        <div
-          id="rightpanel"
-          className="scroll-reveal-element right-scroll-reveal-start "
-        >
-          <p>right InfoPanel</p>
-        </div>
-      </div>
-      <div id="container-scroll-reveal">
-        <div
-          id="leftpanel"
-          className="scroll-reveal-element left-scroll-reveal-start"
-        >
-          <img id="img-scroll-reveal" src={pic} alt="" />
-        </div>
-
-        <div
-          id="rightpanel"
-          className="scroll-reveal-element right-scroll-reveal-start "
-        >
-          <p>right InfoPanel</p>
-        </div>
-      </div>
-      <div id="container-scroll-reveal">
-        <div
-          id="leftpanel"
-          className="scroll-reveal-element left-scroll-reveal-start"
-        >
-          <img id="img-scroll-reveal" src={pic} alt="" />
-        </div>
-
-        <div
-          id="rightpanel"
-          className="scroll-reveal-element right-scroll-reveal-start"
-        >
-          <p>right InfoPanel</p>
-        </div>
-      </div>
-      <div id="container-scroll-reveal">
-        <div
-          id="leftpanel"
-          className="scroll-reveal-element left-scroll-reveal-start"
-        >
-          <img id="img-scroll-reveal" src={pic} alt="" />
-        </div>
-
-        <div
-          id="rightpanel"
-          className="scroll-reveal-element right-scroll-reveal-start"
-        >
-          <p>right InfoPanel</p>
-        </div>
-      </div>
-      {/* <button onClick={handleClick}>add or remove</button>; */}
-      {/* <button onClick={handleNewDiv}>new massive div</button>; */}
     </>
   );
 }
