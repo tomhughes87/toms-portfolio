0c272759e21f9c2f73a1d4e855eb42fb9e2b6d94
Tom Hughes <tomhughes1987@gmail.com>
2022-11-12 13:03:54 +0100

    add hero/call to action compo to skills page

--- src/pages/skills/Index.tsx
+++ src/pages/skills/Index.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import Skills from '../../components/Skills'
-import './index.css'
+import React from "react";
+import Hero from "../../components/Hero/Hero";
+import Skills from "../../components/Skills";
+import "./index.css";
 
 export default function SkillsPage() {
   return (
     // <div>Skills</div>
-
-    <Skills/>
-  )
+    <>
+      <Skills />
+      <Hero />
+    </>
+  );
 }
