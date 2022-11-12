26739b2a7f10d201f28b78a940e8905788073817
Tom Hughes <tomhughes1987@gmail.com>
2022-11-12 13:03:25 +0100

    trun off the gold/silve/bronze skill element

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -9,44 +9,44 @@ export default function Skills ()  {
         'Typescript','React','HTML','CSS','Javascript','Python','Jest',
         'GitHub','Git','Mongodb','Node.js','Flask','WebPack','PostgreSQL','Express','Docker','Django','Redux']
 
-    const techSkillsTop = [
-        'Typescript','React','HTML','CSS','Javascript','Python','Jest',
-        'GitHub','Git',]
-    const techSkillsMid = ['Mongodb','node.js','Flask']
-    const techSkillsLow = ['WebPack','PostgreSQL','Express','Docker','Django','Redux']
+    // const techSkillsTop = [
+    //     'Typescript','React','HTML','CSS','Javascript','Python','Jest',
+    //     'GitHub','Git',]
+    // const techSkillsMid = ['Mongodb','node.js','Flask']
+    // const techSkillsLow = ['WebPack','PostgreSQL','Express','Docker','Django','Redux']
 
-    let techSkillsList = []
-    techSkillsTop.forEach((item,index) => {
-        techSkillsList.push(
-        <Logo
-            key={index}
-            tech={item}
-            path={`/images/skills/${item}.svg`}
-            // tier={'gold'}
-        />)
-    })
+    // let techSkillsList = []
+    // techSkillsTop.forEach((item,index) => {
+    //     techSkillsList.push(
+    //     <Logo
+    //         key={index}
+    //         tech={item}
+    //         path={`/images/skills/${item}.svg`}
+    //         // tier={'gold'}
+    //     />)
+    // })
 
-    let midTechSkillsList = []
-    techSkillsMid.forEach((item,index) => {
-        midTechSkillsList.push(
-        <Logo
-            key={index}
-            tech={item}
-            path={`/images/skills/${item}.svg`}
-            // tier={'silver'}
-        />)
-    })
+    // let midTechSkillsList = []
+    // techSkillsMid.forEach((item,index) => {
+    //     midTechSkillsList.push(
+    //     <Logo
+    //         key={index}
+    //         tech={item}
+    //         path={`/images/skills/${item}.svg`}
+    //         // tier={'silver'}
+    //     />)
+    // })
 
-    let lowTechSkillsList = []
-    techSkillsLow.forEach((item,index) => {
-        lowTechSkillsList.push(
-        <Logo
-            key={index}
-            tech={item}
-            path={`/images/skills/${item}.svg`}
-            // tier={'bronze'}
-        />)
-    })
+    // let lowTechSkillsList = []
+    // techSkillsLow.forEach((item,index) => {
+    //     lowTechSkillsList.push(
+    //     <Logo
+    //         key={index}
+    //         tech={item}
+    //         path={`/images/skills/${item}.svg`}
+    //         // tier={'bronze'}
+    //     />)
+    // })
 
     
     let allTechSkillsList = []
@@ -64,34 +64,34 @@ export default function Skills ()  {
     <div className="container skills-wrapper rounded p-3">
         {/* <hr width='100%'></hr> */}
         {/* <br></br> */}
-        <br></br>
-        <div className="skill-logos gold">
+        {/* <br></br> */}
+        {/* <div className="skill-logos gold"> */}
             {/* <h2 id='skillCardTitle'>Top Skills</h2> */}
             {/* <br></br> */}
-            {techSkillsList}
-        </div>
-        <br></br>
-        <br></br>
+            {/* {techSkillsList} */}
+        {/* </div> */}
+        {/* <br></br> */}
+        {/* <br></br> */}
         {/* <br></br> */}
 
         {/* <hr width='100%'></hr> */}
         {/* <h2>Mid Skills</h2> */}
-        <div className="skill-logos silver">
+        {/* <div className="skill-logos silver">
         {midTechSkillsList}
-        </div>
+        </div> */}
 
         {/* <hr width='100%'></hr> */}
         {/* <h2>Some Skills</h2> */}
-        <br></br>
-        <br></br>
         {/* <br></br> */}
-        <div className="skill-logos bronze">
+        {/* <br></br> */}
+        {/* <br></br> */}
+        {/* <div className="skill-logos bronze">
         {lowTechSkillsList}
-        </div>
+        </div> */}
 
          {/* <hr width='100%'></hr> */}
         {/* <h2>Some Skills</h2> */}
-        <br></br>
+        {/* <br></br> */}
         <br></br>
         {/* <br></br> */}
         <div className="skill-logos white">
