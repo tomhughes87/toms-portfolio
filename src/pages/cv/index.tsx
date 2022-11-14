b50ec31cbeda026cd9e15d374813c2ea44032ebf
Tom Hughes <tomhughes1987@gmail.com>
2022-11-14 22:56:45 +0100

    update the cv pdf viewer to object from iframe,wip

--- src/pages/cv/index.tsx
+++ src/pages/cv/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import pdf from './TomHughesCV.pdf'
-import './index.css'
+import React from "react";
+import pdf from "./TomHughesCV.pdf";
+import "./index.css";
 
 export default function CvPage() {
   return (
-    <div id='cvContainer'>
+    <div id="cvContainer">
+      <br></br>
+      {/* <iframe id="pdf-frame" src={`${pdf}#navpanes=0`}></iframe> */}
+      {/* <iframe src={`${pdf}#navpanes=0`} width="850px" height="1190px"></iframe> */}
+      <br></br>
+      <a id="downloadCVbtn" href={pdf} download="TomHughesCV">
+        Download CV
+      </a>
 
-        <br></br>
-        <iframe src={`${pdf}#navpanes=0`} width="850px" height="1190px"></iframe>
-        <br></br>
-        <a id='downloadCVbtn' href={pdf} download="TomHughesCV">Download CV</a>
+      <object id="pdf-frame" data={pdf} type="application/pdf">
+        <div>No online PDF viewer installed</div>
+      </object>
     </div>
-  )
+  );
 }
